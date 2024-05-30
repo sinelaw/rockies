91b071648045b0ef3cd7b62cadaeb4bab7e74758
Noam Lewis <noam@example.com>
2024-05-30 22:20:27 +0300

    FIRSTA

---
+++ www/index.js
@@ -0,0 +1,13 @@
+import { Universe } from "rockies";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
