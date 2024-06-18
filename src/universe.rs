3a0d63465061da4b0b1940a517ba4167604ecf5e
Noam Lewis <noam@example.com>
2024-06-18 22:00:03 +0300

    IT IS AMAZING

--- src/universe.rs
+++ src/universe.rs
@@ -15,7 +15,7 @@ const MAX_CELLS: usize = 4096;
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
-          web_sys::console::log_1(&format!( $( $t )* ).into())
+        // web_sys::console::log_1(&format!( $( $t )* ).into())
         // println!( $( $t )* );
     };
 }
